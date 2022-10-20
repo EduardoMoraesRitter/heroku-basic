5a2dd1901b3190beea575f333a5adc2a161b040a
Eduardo Ritter <eduardo.moraes.ritter@gmail.com>
2022-10-20 18:39:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ let express = require('express')
 let app = express()
 
 app.get('/',  (req, res) => {
-   console.log("oi 5")
+   console.log("oi 6")
+   console.log("code " + JSON.stringify(req.params))
    res.json("Posso te ajudar")
 })
 
-app.get('/teste?code',  (req, res) => {
+app.get('/teste/:code',  (req, res) => {
    console.log("code " + JSON.stringify(req.params))
    res.json("Posso")
+})
+
+app.get(':code',  (req, res) => {
+   console.log("code " + JSON.stringify(req.params))
+   res.json("ajudar")
 })
 
 app.listen(process.env.PORT || 3000, function () {
