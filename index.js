8e9ca1b11f029f11bd977dcf758912bf500e9efc
Eduardo Ritter <eduardo.moraes.ritter@gmail.com>
2022-10-20 18:18:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/',  (req, res) => {
    res.json("Posso te ajudar")
 })
 
+app.get('/:code',  (req, res) => {
+   console.log("code " + req.params)
+   res.json("Posso")
+})
+
 app.listen(process.env.PORT || 3000, function () {
     console.log("ligado")
 })
